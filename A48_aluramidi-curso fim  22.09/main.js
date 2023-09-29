2b7670d0fcd4908dae64d4935a5b1091fcb686a3
yohanits <132669424+yohanits@users.noreply.github.com>
2023-09-29 08:46:42 -0300

    Add files via upload

---
+++ A48_aluramidi-curso fim  22.09/main.js
@@ -0,0 +1,11 @@
+function tocasompom() {
+    document.querySelector('#som_tecla_pom').play()
+}
+
+const ListaDeTeclas = document.querySelectorAll('.tecla');
+
+ListaDeTeclas[0].onclick = tocasompom;
+
+    
+    
+        
